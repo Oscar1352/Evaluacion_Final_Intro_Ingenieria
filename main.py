cd00467c2d07d0ed81ad9ee81df1d7e66bce032a
Oscar Luna <carolina.151270@gmail.com>
2022-05-03 09:12:24 -0600

    Creación main.py, datos del estudiante
    Se creó un archivo bajo el lenguaje de python con los datos del estudiante

---
+++ main.py
@@ -0,0 +1,8 @@
+#Datos del estudiante
+from traceback import print_tb
+
+
+print("Nombre: Oscar Alejandro Luna Ordoñez")
+print("Carné: 1530622")
+print("Curso: Introducción a la Ingeniería en Informática y Sistemas")
+print("Sección: 01")
